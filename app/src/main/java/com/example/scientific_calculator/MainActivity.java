865c11e14718877f98d14a433f0ad857bd959c11
Josh <74218528+Jmansito@users.noreply.github.com>
2021-11-03 17:28:26 -0600

    Comments in MainActivity.java

--- app/src/main/java/com/example/scientific_calculator/MainActivity.java
+++ app/src/main/java/com/example/scientific_calculator/MainActivity.java
@@ -25,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // Clear button to clear out the form. We may need to set it to where one press clears the current entry, and two presses clears the entire equation.
+    // User types in 34 + 60
+    // One C press would change it to 34 +
+    // Two C presses would change it to an empty form
     public void clearForm(View view){ clearForm(); }
     public void clearForm(){
         binding.results.setText("");
     }
 
+    // Starting to try and make the buttons update the textview
+    // Currently: Not updating with click listener
     public void oneButton(View view){oneButton();}
     public void oneButton() {binding.one.setOnClickListener(v -> binding.results.setText(binding.results.getText() + "1"));}
 
